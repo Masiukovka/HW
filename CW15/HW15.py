77b3ab14f76af99a7d78063786ed58b0803b83c4
Masiukovka <m7544623@gmail.com>
2023-12-18 20:08:40 +0300

    ex. #2 and #3

--- CW15/HW15.py
+++ CW15/HW15.py
@@ -1,15 +1,15 @@
-# 2. Напишите декоратор, замеряет время выполнения функции и кол-во памяти, которое занимает ответ.
-#import datetime
-import time
-
-
-def timer_func(pokupki):
-    def wrapped(*args, **kwargs):
-        start_time = time.perf_counter()
-        pokupki(*args, **kwargs)
-        end_time = time.perf_counter()
-        print(f"Время выполнения функции заняло {end_time - start_time:0.6f} сек.")
-    return wrapped
+# # 2. Напишите декоратор, замеряет время выполнения функции и кол-во памяти, которое занимает ответ.
+# #import datetime
+# import time
+#
+#
+# def timer_func(pokupki):
+#     def wrapped(*args, **kwargs):
+#         start_time = time.perf_counter()
+#         pokupki(*args, **kwargs)
+#         end_time = time.perf_counter()
+#         print(f"Время выполнения функции заняло {end_time - start_time:0.6f} сек.")
+#     return wrapped
 #
 # @timer_func
 # def pokupki(dengi: str, korzina_pokupok: list)-> dict:
@@ -74,9 +74,9 @@ def timer_func(pokupki):
 #
 # # Список, состоящий из элементов s, стоящих на позициях, не кратных 3. позиция 0 кратна 3?
 #
-def pos_ne_kr_3(spisok_chisel):
+# def pos_ne_kr_3(spisok_chisel):
 # #1
-    lst = []
+#     lst = []
 #     # lst.append(spisok_chisel[0])
 #     # lst.extend(list(spisok_chisel[i] for i in range(len(spisok_chisel)) if i % 3 != 0))
 #     # return lst
@@ -84,11 +84,11 @@ def pos_ne_kr_3(spisok_chisel):
 # #2
 #     return [spisok_chisel[i] for i in range(len(spisok_chisel)) if len(spisok_chisel[i]) == 2 and i % 3 != 0]
 #3
-    del spisok_chisel[::3]
-    return spisok_chisel
+    # del spisok_chisel[::3]
+    # return spisok_chisel
 
 #
-spisok_chisel = [22, 22, 45, 60, 122, 225, 66, 66, 369, 1000, 1245]
+# spisok_chisel = [22, 22, 45, 60, 122, 225, 66, 66, 369, 1000, 1245]
 # #               [0    1   2   3    4   5    6   7   8     9     10]
 #
 # print(kvadrat_chisel(spisok_chisel))
@@ -96,7 +96,7 @@ spisok_chisel = [22, 22, 45, 60, 122, 225, 66, 66, 369, 1000, 1245]
 # print(spisok_chetnyh(spisok_chisel))
 # print(spisok_count_nechet(spisok_chisel))
 # print(two_ryadom(spisok_chisel))
-print(pos_ne_kr_3(spisok_chisel))
+# print(pos_ne_kr_3(spisok_chisel))
 
 
 # 3. Напишите декоратор, который будет корректировать результат функции,
@@ -111,43 +111,43 @@ print(pos_ne_kr_3(spisok_chisel))
 #     return wrapped
 
 
-# def korrekt_korzin(pokupki):
-#     def wrapped(*args, **kwargs):
-#         ball = 0
-#         count = 0
-#         for tovar in korzina_pokupok:
-#             count += tovar.get("count") * tovar.get("price")
-#         if count >= 100:
-#             ball = 10
-#         return pokupki(ball, *args, **kwargs)
-#     return wrapped
+def korrekt_korzin(pokupki):
+    def wrapped(*args, **kwargs):
+        ball = 0
+        count = 0
+        for tovar in korzina_pokupok:
+            count += tovar.get("count") * tovar.get("price")
+        if count >= 100:
+            ball = 10
+        return pokupki(ball, *args, **kwargs)
+    return wrapped
 #
 #
 # #@timer_func
-# @korrekt_korzin
-# def pokupki(ball: int, dengi: str, korzina_pokupok: list)-> dict:
-#     dengi = int(dengi)
-#     count = 0
-#     # ball = 0
-#     out = {"status": "", "comment": "", "ball": ""}
-#     for tovar in korzina_pokupok:
-#         count += tovar.get("count") * tovar.get("price")
-#     if count <= int(dengi):
-#         out = {"status": "success",
-#                "comment": "Покупки оплачены. Ваша сдача " + str(dengi - count) + " рублей",
-#                "ball": "Начисленно " + str(ball) + " баллов"}
-#     else:
-#         out = {"status": "fail",
-#                "comment": "Недостаточно средств. Внесите " + str(count - dengi) + " рублей",
-#                "ball": "Начисленно " + str(ball) + " баллов"}
-#     return out
-#
-# dengi = "100"
-# korzina_pokupok = [
-#     {"name": "молоко", "count": 2, "price": 50},
-#     {"name": "сметана", "count": 3, "price": 4},
-#     {"name": "батон", "count": 1, "price": 2},
-#     {"name": "шоколадка", "count": 2, "price": 6},
-#     ]
-# print(pokupki(dengi, korzina_pokupok))
+@korrekt_korzin
+def pokupki(ball: int, dengi: str, korzina_pokupok: list)-> dict:
+    dengi = int(dengi)
+    count = 0
+    # ball = 0
+    out = {"status": "", "comment": "", "ball": ""}
+    for tovar in korzina_pokupok:
+        count += tovar.get("count") * tovar.get("price")
+    if count <= int(dengi):
+        out = {"status": "success",
+               "comment": "Покупки оплачены. Ваша сдача " + str(dengi - count) + " рублей",
+               "ball": "Начисленно " + str(ball) + " баллов"}
+    else:
+        out = {"status": "fail",
+               "comment": "Недостаточно средств. Внесите " + str(count - dengi) + " рублей",
+               "ball": "Начисленно " + str(ball) + " баллов"}
+    return out
+
+dengi = "100"
+korzina_pokupok = [
+    {"name": "молоко", "count": 2, "price": 50},
+    {"name": "сметана", "count": 3, "price": 4},
+    {"name": "батон", "count": 1, "price": 2},
+    {"name": "шоколадка", "count": 2, "price": 6},
+    ]
+print(pokupki(dengi, korzina_pokupok))
 

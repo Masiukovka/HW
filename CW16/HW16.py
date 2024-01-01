59dfc3b1072561aed30e3c9c14ad9d7945056d40
Masiukovka <m7544623@gmail.com>
2024-01-01 21:56:24 +0300

    all ex. are solved

--- CW16/HW16.py
+++ CW16/HW16.py
@@ -4,22 +4,36 @@
 # и вместо выполнения функции возвращать сохранённый результат.
 # После трёх запусков функция должна вызываться вновь, а результат работы функции — вновь кешироваться.
 
-# def counter(func)->list:
-#     def wrapper(*args, **kwargs):
-#         list = []
-#         wrapper.counter += 1
-#         print(f"Вызов функции {wrapper.counter}")
-#         if wrapper.counter < 4:
-#             list.append(kvadrat_chisla(chislo))
-#
-#
-#
-# def kvadrat_chisla(chislo: int)->int:
-#     return chislo ** 2
-#
-# chislo = 22
-#
-# print(kvadrat_chisla(chislo))
+from time import sleep
+
+
+def counter(func):
+    rez = func("gjg")
+    count = 0
+    def wrapper(*args):
+        nonlocal count
+        nonlocal rez
+        count += 1
+        if count <= 3:
+            return rez
+        else:
+            count = 1
+            rez = func(*args)
+            return rez
+    return wrapper
+
+@counter
+def difF(word):
+    sleep(3)
+    return f"{word}"
+
+
+
+print(difF("hgjyg"))
+print(difF("gjhj"))
+print(difF("HKG"))
+print(difF("HKG"))
+print(difF("HKG"))
 
 # 2. Напишите декоратор, который будет считать кол-во вызовов функции в файле.
 
@@ -60,26 +74,32 @@
 # 3. Напишите декоратор, который будет возвращать текст: "Сервер перегружен.
 # Повторите попытку позже", если декорируемая функция выполняется более 3-х секунд.
 
-import time
-
-def timer_func(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        func(*args, **kwargs)
-        end_time = time.perf_counter()
-        if (end_time - start_time) < 3:
-            return print(f"Время выполнения функции подсчета квадратов чисел "
-                         f"с результатом равным: {func(*args, **kwargs)} заняло {end_time - start_time:0.6f} сек."
-                         f"Что меньше допустимого ограничения в 3 сек. Функция выполнена.")
-        else:
-            return print("Сервер перегружен. Повторите попытку позже")
-    return wrapper
-
-
-@timer_func
-def kvadrat_chisel(spisok_chisel: list)->list:
-    return [num ** 2 for num in spisok_chisel]
-
-spisok_chisel = [22, 22, 45, 60, 122, 225, 66, 66, 369, 1000, 1245]
-
-kvadrat_chisel(spisok_chisel)
+# import time
+#
+# def timer_func(func):
+#     def wrapper(*args, **kwargs):
+#         start_time = time.perf_counter()
+#         func(*args, **kwargs)
+#         end_time = time.perf_counter()
+#         if (end_time - start_time) < 3:
+#             return print(f"Время выполнения функции подсчета квадратов чисел "
+#                          f"с результатом равным: {func(*args, **kwargs)} заняло {end_time - start_time:0.6f} сек."
+#                          f"Что меньше допустимого ограничения в 3 сек. Функция выполнена.")
+#         else:
+#             return print("Сервер перегружен. Повторите попытку позже")
+#     return wrapper
+#
+# def rez(func):
+#     def wrapper(*args, **kwargs):
+#         print(func(*args, **kwargs))
+#         return func(*args, **kwargs)
+#     return wrapper
+#
+# @timer_func
+# @rez
+# def kvadrat_chisel(spisok_chisel: list)->list:
+#     return [num ** 2 for num in spisok_chisel]
+#
+# spisok_chisel = [22, 22, 45, 60, 122, 225, 66, 66, 369, 1000, 1245]
+#
+# kvadrat_chisel(spisok_chisel)

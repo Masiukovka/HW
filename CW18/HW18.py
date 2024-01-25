14fe9e50ff6a45f9e57f78cc4a5e942a94894f1a
Masiukovka <m7544623@gmail.com>
2024-01-25 21:14:04 +0300

    HW 19_With SNOW

--- CW18/HW18.py
+++ CW18/HW18.py
@@ -1,23 +1,65 @@
-# numbers = "8 3 -5 42 -1 0 0 -9 4 7 4 -4"
-# out = sorted([int(x) for x in numbers.split(" ")], reverse=True)
-# print(out[0], out[-1])
+# # numbers = "8 3 -5 42 -1 0 0 -9 4 7 4 -4"
+# # out = sorted([int(x) for x in numbers.split(" ")], reverse=True)
+# # print(out[0], out[-1])
+# #
+# # url = "www.codewars.com/katas/?page=1#about"
+# #
+# # print(url.split('#')[0])
 #
-# url = "www.codewars.com/katas/?page=1#about"
+# # Домашнее задание:
+# # 1. Построить иерархию классов, удовлетворяющую следующим условиям
+# # (тематика иерархии на ваше усмотрение):
+# # - Количество классов >= 5.
+# # - Использовать наследование, в т.ч. множественное
+# # - Для каждого класса определить минимум 3 магических метода (на ваш выбор)
+# # - Создать экземпляры для каждого класса
+# # -  Иерархия должна быть логичной и не противоречить принципам ООП.
 #
-# print(url.split('#')[0])
+# # 2. Дополнительно почитать про принципы ООП, SOLID, DRY, KISS.
+#
+# # 3. Почитать про MRO (Diamond problem)
+#
+# class Figure:
+#     def __init__(self, color):
+#         self.color = color
+#
+#     def __str__(self):
+#         return f"Figure {self.color}"
+#
+#     def __repr__(self):
+#         return f"Squere {self.color}"
+#
+# class Squere(Figure):
+#     def __init__(self, side, color):
+#         super().__init__(side, color)
+#         self.side = side
+#         self.color = color
+#
+#     def __str__(self):
+#         return f"Squere {self.color}"
+#
+#     def __repr__(self):
+#         return f"Squere {self.color}"
+#
+#
+# class Circul(Figure):
+#     def __init__(self, side, color):
+#         super().__init__(side, color)
+#         self.side = side
+#         self.color = color
+#
+#     def __str__(self):
+#         return f"Squere {self.color}"
+#
+#     def __repr__(self):
+#         return f"Squere {self.color}"
 
-# Домашнее задание:
-# 1. Построить иерархию классов, удовлетворяющую следующим условиям
-# (тематика иерархии на ваше усмотрение):
-# - Количество классов >= 5.
-# - Использовать наследование, в т.ч. множественное
-# - Для каждого класса определить минимум 3 магических метода (на ваш выбор)
-# - Создать экземпляры для каждого класса
-# -  Иерархия должна быть логичной и не противоречить принципам ООП.
 
-# 2. Дополнительно почитать про принципы ООП, SOLID, DRY, KISS.
+def to_jaden_case(string):
+    t = string.split(" ")
+    return " ".join([i[0].capitalize() + i[1:].lower() for i in t])
 
-# 3. Почитать про MRO (Diamond problem)
-
+string = "How can mirrors be real if our eyes aren't real"
+print(to_jaden_case(string))
 
 
---
+++ CW19/HW19.py
@@ -0,0 +1,54 @@
+# 1. Создать класс Snow. Класс содержит целое число - количество снежинок.
+# Класс включает методы перегрузки арифметических операторов сложения, вычитания,
+# умножения и деления. Код этих методов должен выполнять увеличение
+# или уменьшение количества снежинок на число n или в n раз.
+# Класс также включает метод makeSnow(), который принимает сам объект и число
+# снежинок в ряду, а возвращает строку вида
+# "*****\n*****\n*****…",  где количество снежинок между '\n' равно переданному аргументу,
+# а количество рядов вычисляется, исходя из общего количества снежинок.
+
+class Snow:
+    all_snow = 25
+    object_snow = "*"
+
+    def __init__(self, operant_snow, n, str_snow):
+        self.operant_snow = operant_snow
+        self.n = n
+
+
+    def umnozhenie(n):
+        return Snow.all_snow * n
+
+    def delenie(n):
+        return round(Snow.all_snow / n)
+
+    def slozhenie(n):
+        return round(Snow.all_snow + n)
+
+    def vychitanie(n):
+        return round(Snow.all_snow - n)
+
+    def makeSnow(operant_snow, n):
+        str_snow = ""
+        if operant_snow == "*":
+            str_snow = Snow.umnozhenie(n) * Snow.object_snow
+        elif operant_snow == "/":
+            str_snow = Snow.delenie(n) * Snow.object_snow
+        elif operant_snow == "+":
+            str_snow = Snow.slozhenie(n) * Snow.object_snow
+        elif operant_snow == "-":
+            str_snow = Snow.vychitanie(n) * Snow.object_snow
+        return "\\n".join(str_snow[0:n] for t in range(0, len(str_snow), n))
+
+
+
+snezhok_um = Snow.makeSnow("*", 3)
+snezhok_delenie = Snow.makeSnow("/", 2)
+snezhok_slozhenie = Snow.makeSnow("+", 2)
+snezhok_vychital = Snow.makeSnow("-", 5)
+
+
+print(snezhok_um)
+print(snezhok_delenie)
+print(snezhok_slozhenie)
+print(snezhok_vychital)
